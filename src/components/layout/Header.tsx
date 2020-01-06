413355fb69492337950d68ab20851b4c300f115a
kalil reis de sisto <kalilreisdesisto@gmail.com>
2020-01-05 23:46:33 -0300

    search bar component created

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
-import logo from "../../icons/logo.svg";
+import logoIcon from "../../icons/logo.svg";
+import searchIcon from "../../icons/search.svg";
+import removeIcon from "../../icons/remove.svg";
 import styled from "styled-components";
 
 interface HeaderProps {
@@ -12,7 +14,6 @@ export const Header: React.FC<HeaderProps> = ({
   handleSearchValue
 }) => {
   const [search, setSearch] = useState("");
-
   const [timer, setTimer] = useState();
 
   useEffect(() => {
@@ -26,14 +27,20 @@ export const Header: React.FC<HeaderProps> = ({
   return (
     <StyledHeader>
       <div className="top-header inline">
-        <img src={logo} width={"100px"} height={"50px"} />
-        <div>
+        <img src={logoIcon} width={"100px"} height={"50px"} />
+        <StyledSearchBar>
+          <div className={"search-icon"}>
+            <img src={searchIcon} />
+          </div>
           <input
             placeholder="search"
             type="text"
             onChange={e => setSearch(e.target.value)}
           />
-        </div>
+          <div className={"remove-icon"}>
+            <img className={search ? "" : "hide"} src={removeIcon} />
+          </div>
+        </StyledSearchBar>
       </div>
       <div className={"search-display"}>
         <p>{search ? search : "Lista de produtos"}</p>
@@ -45,6 +52,49 @@ export const Header: React.FC<HeaderProps> = ({
   );
 };
 
+const StyledSearchBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 1.5rem;
+
+  input {
+    border: 1px solid black;
+    border-left: none;
+    border-right: none;
+  }
+
+  .search-icon {
+    border: 1px solid black;
+    border-top-left-radius: 15px;
+    border-bottom-left-radius: 15px;
+    border-right: none;
+    width: 3rem;
+    display: flex;
+    justify-content: center;
+
+    img {
+      width: 10px;
+    }
+  }
+
+  .remove-icon {
+    border: 1px solid black;
+    border-top-right-radius: 15px;
+    border-bottom-right-radius: 15px;
+    border-left: none;
+    width: 3rem;
+    display: flex;
+    justify-content: center;
+
+    img {
+      width: 12px;
+    }
+
+    img.hide {
+      display: none;
+    }
+  }
+`;
 const StyledHeader = styled.header`
   display: flex;
   flex-direction: column;
